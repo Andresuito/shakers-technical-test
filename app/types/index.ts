6adc01bfd1a9187c1a456d5d45967d71abd38073
Andresuito <barrancorios@gmail.com>
2025-07-19 20:28:03 +0200

    fix: remove comment

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,8 +1,3 @@
-// API Types
 export type { ApiData, FilterOption, FilterOptions, FilterState } from './api';
-
-// Project Types  
 export type { Project, ProjectBudget, ProjectOrganization, SearchParams } from './project';
-
-// Component Types
 export type { Position, TeamPositionsProps, FAQ, FAQSectionProps, ProjectDetail } from './components';
